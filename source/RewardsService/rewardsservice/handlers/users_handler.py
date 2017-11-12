1f09b52e0e9550c371d1dd08fe11b8958f688f81
Ted Zellers <ted@tedzellers.com>
2017-11-12 14:07:20 -0500

    search for user by email

--- source/RewardsService/rewardsservice/handlers/users_handler.py
+++ source/RewardsService/rewardsservice/handlers/users_handler.py
@@ -6,25 +6,56 @@ from tornado.gen import coroutine
 
 
 class UsersHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        # If we were building a public API, this would need to be adjusted
+        self.set_header("Access-Control-Allow-Origin", "http://localhost:8000")
 
     @coroutine
     def get(self):
-        client = MongoClient("mongodb", 27017)
-        db = client["Rewards"]
-        db.users.remove()
-        db.users.insert({
-            "email": "bob@thebobsociety.com",
-            "points": 101,
-            "tier": "A",
-            "tier_name": "5% off purchase",
-            "next_tier": "B",
-            "next_tier_name": "10% off purchase",
-            "next_tier_progress": ".5",
+        if self.request.method == "GET":
+            url_args = self.request.path.split("/")
+            query = {}
+            if len(url_args) == 3:
+                email = url_args[2]
+                query = {"email":email}
 
-            })
-        users = list(db.users.find({}, {"_id": 0}))
-        # self.write(json.dumps(users))
-        url_args = self.request.path.split("/")
-        if len(url_args) == 3:
-            uid = int(url_args[2])
-        self.write(json.dumps(users) + json.dumps(uid))
+            client = MongoClient("mongodb", 27017)
+            db = client["Rewards"]
+            db.users.remove()
+            db.users.insert({
+                "email": "bob@thebobsociety.com",
+                "points": 101,
+                "tier": "A",
+                "tier_name": "5% off purchase",
+                "next_tier": "B",
+                "next_tier_name": "10% off purchase",
+                "next_tier_progress": ".5",
+
+                })
+            db.users.insert({
+                "email": "robert@thebobsociety.com",
+                "points": 101,
+                "tier": "A",
+                "tier_name": "5% off purchase",
+                "next_tier": "B",
+                "next_tier_name": "10% off purchase",
+                "next_tier_progress": ".5",
+
+                })
+            users = list(db.users.find(query, {"_id": 0}))
+            self.write(json.dumps(users))
+        elif self.request.method == "PUT":
+            self.write(json.dumps(self.request))
+        else:
+            self.write(json.dumps({"error":"404"}))
+
+
+
+
+
+
+
+
+
+
+#
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,8 +1,11 @@
 from handlers.rewards_handler import RewardsHandler
 from handlers.users_handler import UsersHandler
 
+# Email regex adapted from http://www.regular-expressions.info/email.html
+# Not perfect but 99% good. Could be revisited.
+
 url_patterns = [
     (r'/rewards', RewardsHandler),
     (r'/users', UsersHandler),
-    (r'/users\/[0-9]+', UsersHandler),
+    (r'/users\/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]', UsersHandler),
 ]
