52e0b1c35af217ab729514b6e141e990cb2cbe0b
Ted Zellers <ted@tedzellers.com>
2017-11-13 15:01:51 -0500

    allow new user creation; handle purchases with cents and purchases < ; show tier progress

--- source/RewardsService/rewardsservice/handlers/users_handler.py
+++ source/RewardsService/rewardsservice/handlers/users_handler.py
@@ -25,84 +25,51 @@ class UsersHandler(tornado.web.RequestHandler):
 
             client = MongoClient("mongodb", 27017)
             db = client["Rewards"]
-            # db.users.remove()
-            # db.users.insert_one({
-            #     "email": "jimbob@thebobsociety.com",
-            #     "points": 102,
-            #     "tier": "A",
-            #     "tier_name": "5% off purchase",
-            #     "next_tier": "B",
-            #     "next_tier_name": "10% off purchase",
-            #     "next_tier_progress": ".5",
-            #
-            #     })
-            # db.users.insert_one({
-            #     "email": "bobby@thebobsociety.com",
-            #     "points": 102,
-            #     "tier": "A",
-            #     "tier_name": "5% off purchase",
-            #     "next_tier": "B",
-            #     "next_tier_name": "10% off purchase",
-            #     "next_tier_progress": ".5",
-            #
-            #     })
-            # db.users.insert_one({
-            #     "email": "bob@thebobsociety.com",
-            #     "points": 103,
-            #     "tier": "A",
-            #     "tier_name": "5% off purchase",
-            #     "next_tier": "B",
-            #     "next_tier_name": "10% off purchase",
-            #     "next_tier_progress": ".5",
-            #
-            #     })
-            # db.users.insert_one({
-            #     "email": "robert@thebobsociety.com",
-            #     "points": 101,
-            #     "tier": "A",
-            #     "tier_name": "5% off purchase",
-            #     "next_tier": "B",
-            #     "next_tier_name": "10% off purchase",
-            #     "next_tier_progress": ".5",
-            #
-            #     })
             users = list(db.users.find(query, {"_id": 0}))
             self.write(json.dumps(users))
 
     @coroutine
     def put(self):
         total = self.get_body_argument('total');
-        if (int(total) < 0):
-            return
+        if (int(float(total)) < 0):
+            total = 0   #we'll make the record anyway
         url_args = self.request.path.split("/")
         if len(url_args) != 3:
             return
 
-        points = int(total) #no credit for cents
+        points = int(float(total)) #no credit for cents
         email = url_args[2]
 
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
         query = {"email":email}
         user = db.users.find_one(query, {"_id": 0})
-        new_points = int(user['points']) + points
+        if user is None:
+            new_points = points
+            db.users.insert_one({
+                "email": email,
+                "points": 0,
+                })
+        else:
+            new_points = int(user['points']) + points
 
-        # Defaults / out of range fillers
-        # highest_achieved = {"rewardName": "None", "tier": "None", "points":})
-        # next_tier = {"rewardName": "None", "tier": "None"})
-
+        #Hack -- for some reason these reward tiers are getting deleted. Not sure why.
+        r = db.rewards.find({'rewardName':'None'})
+        if r.count() == 0:
+            db.rewards.insert({"points": -1, "rewardName": "None", "tier": "None", "placeholder": "True"})
+            db.rewards.insert({"points": 999999999999999, "rewardName": "None", "tier": "None", "placeholder": "True"})
 
         # Get best matching tier
-        for r in db.rewards.find({'points': {'$lt': new_points}}, {"_id": 0}).sort("points",pymongo.DESCENDING).limit(1):
+        for r in db.rewards.find({'points': {'$lte': new_points}}, {"_id": 0}).sort("points",pymongo.DESCENDING).limit(1):
             highest_achieved = r
         for r in db.rewards.find({'points': {'$gt': new_points}}, {"_id": 0}).sort("points",pymongo.ASCENDING).limit(1):
             next_tier = r
 
-        if next_tier['rewardName'] == 'None':
-            progress = 'None'
-        else :
-            progress = '.1'
-
+        if next_tier['rewardName'] == 'None' or new_points == 0:
+            progress = 0
+        else:
+            progress = (new_points - highest_achieved['points'])/(next_tier['points'] - highest_achieved['points'])
+            progress = round(progress*100) #return a %
 
         new_data = {
             'points':new_points,
@@ -110,12 +77,13 @@ class UsersHandler(tornado.web.RequestHandler):
             "tier_name": highest_achieved['rewardName'],
             "next_tier": next_tier['tier'],
             "next_tier_name": next_tier['rewardName'],
-            "next_tier_progress": ".5", #@todo
+            "next_tier_progress": progress,
         }
         db.users.update_one(query, {'$set': new_data})
 
-        self.write(json.dumps(new_data))
+        # self.write(json.dumps(new_data))
         # self.write(json.dumps({"put":"ok","oldpts":points,"newpts":new_points}))
+        self.write(json.dumps({"put":"ok", "ha": highest_achieved, "nt":next_tier}))
 
 
     @coroutine
